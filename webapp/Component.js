37353b99d000236b132d4ebc3ff1746b23f9de31
HelloMua <jmua333@gmail.com>
2021-09-14 15:42:48 +0900

    Modify Dialog
    Modify Dialog

--- webapp/Component.js
+++ webapp/Component.js
@@ -40,7 +40,9 @@ sap.ui.define([
 			this.setModel(oModel);
 
 			// set dialog
-			this._helloDialog = new HelloDialog(this.getRootControl());
+			this.rootControlLoaded().then(function() {
+				this._helloDialog = new HelloDialog(this.getRootControl());
+			}.bind(this));
 
 			// create the views based on the url/hash
 			this.getRouter().initialize();
